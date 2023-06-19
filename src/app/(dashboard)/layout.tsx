4ec15d71ba8b2d18ec0ab940922a0efc8d198ed8
cluab <cluab11222@gmail.com>
2023-06-19 08:10:31 +0300

    add react-modal div

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -12,7 +12,7 @@ export default function DashboardRootLayout({ children }) {
                     <Sidebar />
                     {children}
                 </GlassPane>
-
+                <div id="modal"></div>
             </body>
         </html>
     )
